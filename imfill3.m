e5a2fedd1386b6f08335294e3236747ed6182a21
ruijie.shao <ruijie.shao@gmail.com>
2018-12-26 17:36:46 -0800

    imfill3

---
+++ imfill3.m
@@ -0,0 +1,10 @@
+function A = imfill3(A)
+    %fills in holes in a 3D array by performing imfill in 3 dimensions
+    
+    for i=1:3
+        for j=1:size(A,3)
+            A(:,:,j)=imfill(A(:,:,j),'holes');
+        end
+        A=shiftdim(A,1);
+    end
+end
